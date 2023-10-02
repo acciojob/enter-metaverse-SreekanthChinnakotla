e2f59bc1ac0964149657f3e09e5d69282ca642f8
SreekanthChinnakotla <134213780+SreekanthChinnakotla@users.noreply.github.com>
2023-10-02 22:02:07 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,5 @@
 let ptag = document.getElementById("status");
 let butn = document.getElementById("enterBtn");
 function clicked(){
-	 let h1element= document.createElement("h1");
-	 h1element.innerText = "Entered Metaverse";
-	h1element.id=ptag.id;
-	
-	 ptag.parentNode.replaceChild(h1element, ptag);
+	ptag.innerHTML="<h1>Entered Meteverse</h1>"
  };
