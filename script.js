c8e874ef94a27b927a30508f3c264eafb6fe7fed
SreekanthChinnakotla <134213780+SreekanthChinnakotla@users.noreply.github.com>
2023-10-02 19:38:35 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,16 +1,9 @@
-     var convertButton = document.getElementById("enterBtn");
-        convertButton.addEventListener("click", function() {
-            // Get the existing p tag
-            var pElement = document.getElementById("status");
+let ptag = document.getElementById("status");
+let ptext = ptag.innerText;
+let butn = document.getElementById("enterBtn");
 
-            // Create a new h1 element
-            var h1Element = document.createElement("h1");
-
-            // Copy the content and attributes from the p tag to the new h1 tag
-            h1Element.textContent ="Entered Metaverse";
-            h1Element.id = pElement.id;
-            h1Element.className = pElement.className;
-
-            // Replace the p tag with the new h1 tag
-            pElement.parentNode.replaceChild(h1Element, pElement);
-        });
+ butn.addEventListener("click",()=>{
+	 let h1element= document.createElement("h1");
+	 h1element.innerText = "Entered Metaverse";
+	 ptag.parentNode.replaceChild(h1element, ptag);
+ })
