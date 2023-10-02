17fcae6b81c27bf9ae2fb81cbb44bed88e37f52c
SreekanthChinnakotla <134213780+SreekanthChinnakotla@users.noreply.github.com>
2023-10-02 19:00:51 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,20 +1,12 @@
-     var statusElement = document.getElementById("status");
-        var enterButton = document.getElementById("enterBtn");
-
-        // Add a click event listener to the button
-        enterButton.addEventListener("click", function() {
-            // Change the text of the p tag
-            statusElement.textContent = "Entered Metaverse";
-        });    // Get references to the HTML elements
         var statusElement = document.getElementById("status");
         var enterButton = document.getElementById("enterBtn");
 
-        // Add a click event listener to the button
-        enterButton.addEventListener("click", function() {
+
+ function clicked() {
             // Create a new h1 element
             var newHeader = document.createElement("h1");
             newHeader.textContent = "Entered Metaverse";
 
             // Replace the p tag with the new h1 element
             statusElement.parentNode.replaceChild(newHeader, statusElement);
-        });
+        }
