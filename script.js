0d6055205070e7f7bcd311d34ce8a54d55213c5c
SreekanthChinnakotla <134213780+SreekanthChinnakotla@users.noreply.github.com>
2023-10-02 19:59:09 +0530

    created a submission

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ let butn = document.getElementById("enterBtn");
 function clicked(){
 	 let h1element= document.createElement("h1");
 	 h1element.innerText = "Entered Metaverse";
-	 ptag.parentNode.replaceChild(h1element, ptag);
 	h1element.id=ptag.id;
+	
+	 ptag.parentNode.replaceChild(h1element, ptag);
  };
