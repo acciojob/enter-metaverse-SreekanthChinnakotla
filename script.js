d2e9104e1032f4f79e02dbb542f3232740ecf67e
SreekanthChinnakotla <134213780+SreekanthChinnakotla@users.noreply.github.com>
2023-10-02 18:54:36 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
-//your JS code here. If required.
-function changeingtext(){
-let element = document.getElementById("status");
-	
-	element.innerText="Entered Metaverse";
-}
+     var statusElement = document.getElementById("status");
+        var enterButton = document.getElementById("enterBtn");
+
+        // Add a click event listener to the button
+        enterButton.addEventListener("click", function() {
+            // Change the text of the p tag
+            statusElement.textContent = "Entered Metaverse";
+        });
